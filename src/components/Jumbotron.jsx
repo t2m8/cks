2fad6051eb7f7b4750a943deae7a8822909bea51
t2m8 <krbrozen@gmail.com>
2023-12-02 02:42:06 +0700

    nambahken hiji gambar sareng warna na

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,14 +1,15 @@
 import LgJ from '../assets/images__6_-removebg-preview.svg'
+import sttang from '../assets/sttang.jpeg'
 
 export default function Jumbotron (){
   return(
-    <section className="mt-14 w-full">
-      <div className="px-4 mx-auto flex flex-col md:flex md:justify-between md:items-center">
+    <section className="mt-16 md:mt-24 bg-cyan-500 w-full">
+      <div className="px-4 mx-auto flex flex-wrap md:flex md:justify-between md:items-center">
         <div className="pt-6">
         <h1>Selamat Datang Di website Cks</h1>
         </div>
-        
-        <img className="" src={LgJ} alt="cks" />
+        {/*w-1/2 h-2/5*/}
+        <img className="" src={sttang} alt="cks" />
       </div>
     </section>
     )
